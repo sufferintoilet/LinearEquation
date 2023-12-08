716e8d64ad94106e1ad0a1da92e1ebcd73ecfdcd
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-09 00:05:08 +0900

    1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        Console.WriteLine("enter a");
+        Double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("enter b");
+        Double b = Convert.ToDouble(Console.ReadLine());
+
+        if(a!=0)
+            Console.WriteLine("Pt co nghiem" + -b/a);
+        else if(b==0)
+            Console.WriteLine("vo so nghiem");
+        else 
+            Console.WriteLine("vo nghiem");
+        
+       
+    }
+}
